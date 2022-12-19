c3926f29de5550f3d8f8c3d3dd39c6ce18efe188
blauert <95693202+blauert@users.noreply.github.com>
2022-12-19 11:30:04 +0100

    Silence output

--- 03 Rucksack Reorganization/rucksack_reorganization.sh
+++ 03 Rucksack Reorganization/rucksack_reorganization.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 # https://adventofcode.com/2022/day/3
 
-TMPFILE='/tmp/rr.py'
-
-rm -f $TMPFILE
-
+TMPFILE='/tmp/rucksack_reorganization.py'
 URL='https://raw.githubusercontent.com/blauert/Advent-of-Code-2022/master/03%20Rucksack%20Reorganization/rucksack_reorganization.py'
-
-wget -O $TMPFILE $URL
+wget -O $TMPFILE $URL &> /dev/null
 
 echo 'Solution:'
-
 python3 $TMPFILE
 
 rm -f $TMPFILE
