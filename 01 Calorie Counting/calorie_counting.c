fa80541a9ce555dc231d8f92ec77df2848c78968
blauert <95693202+blauert@users.noreply.github.com>
2023-01-09 18:07:07 +0100

    Refactor

--- 01 Calorie Counting/calorie_counting.c
+++ 01 Calorie Counting/calorie_counting.c
@@ -19,21 +19,22 @@ int main(void) {
     // Line buffer used by fgets()
     char buff[10];
     // End of line; used by strtol()
-    char *endp = "\r";
+    char end = '\r';
+    char *endp = &end;
 
     // Calories accumulator (per elf)
-    uint sum = 0;
+    unsigned int sum = 0;
     // Max sum
-    uint max = 0;
+    unsigned int max = 0;
     //// Array of all sums
-    uint sums[1000];
-    uint len_sums = 0;
+    unsigned int sums[1000];
+    size_t len_sums = 0;
 
     while (1) {
         // https://en.cppreference.com/w/c/io/fgets
         cp = fgets(buff, 10, fp);
 
-        if (!cp || *cp == '\r') {
+        if (!cp || *cp == end) {
             if (sum > max) max = sum;
             sums[len_sums] = sum;
             if (!cp) break;
@@ -42,7 +43,7 @@ int main(void) {
         }
         else {
             // https://en.cppreference.com/w/c/string/byte/strtol
-            uint num = strtol(cp, &endp, 10);
+            unsigned int num = strtol(cp, &endp, 10);
             sum += num;
         }
     }
@@ -56,10 +57,10 @@ int main(void) {
     // Part 2
 
     // https://en.cppreference.com/w/c/algorithm/qsort
-    qsort(sums, len_sums+1, sizeof(int), cmpfunc);
+    qsort(sums, len_sums+1, sizeof(sums[0]), cmpfunc);
 
-    uint sum_max3 = 0;
-    for (uint i = 0; i < 3; i++) {
+    unsigned int sum_max3 = 0;
+    for (unsigned int i = 0; i < 3; i++) {
         sum_max3 += sums[i];
     }
     printf("Part 2: %d\n", sum_max3);
