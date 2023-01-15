0be6e27b13319fd96d15113cfc8790b0aecc4e30
blauert <95693202+blauert@users.noreply.github.com>
2023-01-15 17:28:05 +0100

    Add day 6 C solution

---
+++ 06 Tuning Trouble/tuning_trouble.c
@@ -0,0 +1,63 @@
+// https://adventofcode.com/2022/day/6
+// Run: $ make
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int distinct_chars(FILE* fp, int num_chars) {
+    int countmap[26] = {0};  // indices 0-25 -> letters a-z
+    int unique_chars = 0;
+    int curr_char = 0;
+    char c;
+
+    while (1) {
+        curr_char++;
+        c = fgetc(fp);
+        if(feof(fp)) break;
+
+        // take one char
+        countmap[c-'a']++;
+        if (countmap[c-'a'] == 1) {
+            unique_chars++;
+        }
+        else if (countmap[c-'a'] == 2) {
+            unique_chars--;
+        }
+
+        if (curr_char > num_chars) {
+            // drop one char
+            fseek(fp, -num_chars-1, SEEK_CUR);
+            c = fgetc(fp);
+            countmap[c-'a']--;
+            if (countmap[c-'a'] == 0) {
+                unique_chars--;
+                }
+            else if (countmap[c-'a'] == 1) {
+                unique_chars++;
+            }
+            fseek(fp, num_chars, SEEK_CUR);
+        }
+        if (unique_chars >= num_chars) {
+            return curr_char;
+        }
+    }
+}
+
+int main(void) {
+    /*
+    const char INPUT_FILE[] = "test_input.txt";
+    */
+    const char INPUT_FILE[] = "real_input.txt";
+
+    FILE *fp = fopen(INPUT_FILE, "r");
+    
+    int part1 = distinct_chars(fp, 4);
+    printf("Part 1: %d\n", part1);
+
+    rewind(fp);
+
+    int part2 = distinct_chars(fp, 14);
+    printf("Part 2: %d\n", part2);
+
+    fclose(fp);
+}
--- README.md
+++ README.md
@@ -7,8 +7,8 @@ https://adventofcode.com/2022
 |[2](./02%20Rock%20Paper%20Scissors/)|⭐⭐||⭐⭐|⭐⭐|||
 |[3](./03%20Rucksack%20Reorganization/)|⭐⭐||⭐⭐|💫|⭐⭐|SQL: Recursive string splitting|
 |[4](./04%20Camp%20Cleanup)|⭐⭐||⭐⭐||||
-|[5](./05%20Supply%20Stacks/)|⭐⭐||||⭐⭐|C: malloc|
-|[6](./06%20Tuning%20Trouble/)|⭐⭐|⭐⭐|⭐⭐||||
+|[5](./05%20Supply%20Stacks/)|⭐⭐||||⭐⭐|C: malloc & pointers|
+|[6](./06%20Tuning%20Trouble/)|⭐⭐|⭐⭐|⭐⭐||⭐⭐||
 |[7](./07%20No%20Space%20Left%20On%20Device/)|⭐⭐|⭐⭐||||Tree; @dataclass|
 |[8](./08%20Treetop%20Tree%20House/)|⭐⭐|⭐⭐||||Looking backwards and forwards 🌳|
 |[9](./09%20Rope%20Bridge/)|⭐⭐|||||Rope tail follows head|
